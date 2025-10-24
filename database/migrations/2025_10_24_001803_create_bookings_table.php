00b541c7bdcb1bf6595b3c581310a4c1aff1fe6e
faaiqa <abdillahfaaiq@gmail.com>
2025-10-24 07:24:29 +0700

    Part 3 - Database migration, models, and admin seeder

---
+++ database/migrations/2025_10_24_001803_create_bookings_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+{
+    Schema::create('bookings', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+        $table->foreignId('buku_id')->constrained('bukus')->onDelete('cascade');
+        $table->date('tanggal_booking');
+        $table->date('batas_ambil')->nullable();
+        $table->enum('status', ['pending', 'diambil', 'selesai', 'batal'])->default('pending');
+        $table->timestamps();
+    });
+}
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bookings');
+    }
+};
